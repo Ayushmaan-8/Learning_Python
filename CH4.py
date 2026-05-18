92fc0dedc0024879101bbf403c127cd8b51b13e8
Ayushmaan-8 <ayushmaanjha08@gmail.com>
2026-05-18 18:31:44 +0530

    list and tuples
    Solved bunch of conceptual problems for list and tuples

---
+++ CH4.py
@@ -0,0 +1,28 @@
+#2)Write a program to accept marks of 6 students and display them in a sorted manner.
+# Marks= []  
+# a1 = int(input("Enter marks of a1 student:"))
+# Marks.append(a1)
+# a2 = int(input("Enter marks of a2 student:"))
+# Marks.append(a2)
+# a3 = int(input("Enter marks of a3 student:"))
+# Marks.append(a3)
+# a4 = int(input("Enter marks of a4 student:"))
+# Marks.append(a4)
+# a5 = int(input("Enter marks of a5 student:"))
+# Marks.append(a5)
+# a6 = int(input("Enter marks of a6 student:"))
+# Marks.append(a6)
+# #Marks.sort()
+# print(Marks)
+# #or
+# print(sorted(Marks))
+
+
+#4) Write a program to sum a list with 4 numbers.
+l1 = [ 3,4,5,6 ]
+print ("sum of l1 is", sum(l1))
+
+
+# 5) Write a program to count the number of zeros in the following tuple:
+# a = (7, 0, 8, 0, 0, 9)
+# print(a.count(0))
