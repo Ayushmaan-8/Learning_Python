6abefc2e03ad9479a5c1edd7697136c4358c0471
Ayushmaan-8 <ayushmaanjha08@gmail.com>
2026-05-18 16:36:44 +0530

    basic hardcoded calculator

---
+++ main.py
@@ -0,0 +1,7 @@
+print("this is a calculator")
+a = 45 
+b = 86
+print ("result of addition\n", a+b) 
+print ("result of subtraction\n", a-b)
+print ("result of devision\n", a//b)
+print ("result of multiplication\n", a*b)
